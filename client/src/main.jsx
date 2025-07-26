a616a5bbb8574f182ebecc4895681ee0a4db54a0
imraushankr <raushan.kumar.19u@gmail.com>
2025-07-26 13:05:28 +0530

    Refactor code structure for improved readability and maintainability

--- client/src/main.jsx
+++
@@ -1,6 +0,0 @@
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import MainRoutes from "./routers/MainRoutes.jsx";
-import { RouterProvider } from "react-router-dom";
-
-createRoot(document.getElementById("root")).render(<RouterProvider router={MainRoutes}/>);
